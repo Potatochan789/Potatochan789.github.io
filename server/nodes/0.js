8dc9025c1990c4a9ace22cde305f48b2888889f3
Potatochan789 <123514992+Potatochan789@users.noreply.github.com>
2023-09-20 14:16:22 -0400

    Delete server directory

--- server/nodes/0.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 0;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/layout.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/0.61dbbcdc.js","_app/immutable/chunks/scheduler.8746962a.js","_app/immutable/chunks/index.914b573a.js"];
-export const stylesheets = [];
-export const fonts = [];
--- server/nodes/1.js
+++
@@ -1,8 +0,0 @@
-
-
-export const index = 1;
-let component_cache;
-export const component = async () => component_cache ??= (await import('../entries/fallbacks/error.svelte.js')).default;
-export const imports = ["_app/immutable/nodes/1.df8560f6.js","_app/immutable/chunks/scheduler.8746962a.js","_app/immutable/chunks/index.914b573a.js","_app/immutable/chunks/singletons.6a0b5e08.js"];
-export const stylesheets = [];
-export const fonts = [];
