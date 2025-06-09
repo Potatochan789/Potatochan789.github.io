920085e2840fff5ec3af35f3722012920c162efa
Potatochan789 <123514992+Potatochan789@users.noreply.github.com>
2025-06-08 20:27:23 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -82,7 +82,7 @@ def get_all_replay_links(url: str):
             links = soup.find_all("a", href=True)
             
             for link in links:
-                if not ("https://replay.pokemonshowdown.com/smogtours-gen9ou" in link["href"] or "https://replay.pokemonshowdown.com/gen9ou" in link["href"]):
+                if not ("https://replay.pokemonshowdown.com/smogtours-gen9cap" in link["href"] or "https://replay.pokemonshowdown.com/gen9cap" in link["href"]):
                     continue
                 if correct_links.count(link["href"]) > 0:
                     continue
